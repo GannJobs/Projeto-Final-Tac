67c100a7b230257ebd89662ef27b5c63340a83ba
GannJobs <linksyamato@gmail.com>
2022-12-26 11:51:05 -0300

    adicionado 2 skills da heroina

--- assets/classes/hero.lua
+++ assets/classes/hero.lua
@@ -13,6 +13,10 @@ function Hero:new()
     self.direcao = 1 -- 1 direita / -2 esquerda
     self.movimento = false
 
+    -- sprites
+
+    -- movimentacao
+
     self.parada = love.graphics.newImage("assets/imagens/personagem/Parada.png")
     local grid = anim.newGrid(32, 32, self.parada:getWidth(), self.parada:getHeight())
     self.animationP = anim.newAnimation(grid('1-1', 1), 0.1)
@@ -21,17 +25,31 @@ function Hero:new()
     local grid = anim.newGrid(32, 32, self.andando:getWidth(), self.andando:getHeight())
     self.aniHeroMove = anim.newAnimation(grid('1-3', 1, '1-3', 2, '1-3', 3, '1-1', 4), 0.1)
 
+    -- ataques
+
+    -- poderes
+    self.Skill1 = false
+    self.Skill01 = love.graphics.newImage("assets/imagens/personagem/Skill 01.png")
+    local grid = anim.newGrid(64, 64, self.Skill01:getWidth(), self.Skill01:getHeight())
+    self.aniSkill01 = anim.newAnimation(grid('1-5', 1, '1-5', 2, '1-5', 3, '1-5', 4, '1-5', 5, '1-3', 6), 0.1)
+
+    self.Skill2 = false
+    self.Skill02 = love.graphics.newImage("assets/imagens/personagem/Skill 02.png")
+    local grid = anim.newGrid(64, 64, self.Skill02:getWidth(), self.Skill02:getHeight())
+    self.aniSkill02 = anim.newAnimation(grid('1-6', 1, '1-6', 2, '1-6', 3, '1-6', 4, '1-6', 5, '1-6', 6, '1-4', 7), 0.1)
+
     -- controladores
 
     self.tempo = 0
-    self.aux = 1
+    self.tempoaux1 = 0
+    self.tempoaux2 = 0
 
 end
 
 function Hero:update(dt)
-
     self.tempo = self.tempo + dt
     self:mov(dt)
+    self:Skills(dt)
 
     self.posicao.x = self.x
     self.posicao.y = self.y
@@ -39,11 +57,55 @@ function Hero:update(dt)
 end
 
 function Hero:draw()
+
     if self.movimento == true then
         self.aniHeroMove:draw(self.andando, self.x, self.y, 0, self.direcao, 1, 16, 0)
     else
         love.graphics.draw(self.parada, self.x, self.y, 0, self.direcao, 1, 16, 0)
     end
+
+    if self.Skill1 == true then
+        self.aniSkill01:draw(self.Skill01, x1, y1, 0, 1, 1, 32, 0)
+    end
+
+    if self.Skill2 == true then
+        self.aniSkill02:draw(self.Skill02, x2, y2, 0, 1, 1, 32, 0)
+    end
+
+end
+
+function Hero:Skills(dt)
+
+    if love.keyboard.isDown("k") and self.Skill1 == false then
+        x1 = self.x
+        y1 = self.y
+        self.tempoaux1 = self.tempo
+        self.Skill1 = true
+    end
+
+    if self.Skill1 == true then
+        self.aniSkill01:update(dt)
+        if(self.tempo > self.tempoaux1 + 2.7) then
+            self.tempoaux1 = 0
+            self.Skill1 = false
+        end
+    end
+
+    if love.keyboard.isDown("l") and self.Skill2 == false then
+        x2 = self.x
+        y2 = self.y
+        self.tempoaux2 = self.tempo
+        self.Skill2 = true
+    end
+
+    if self.Skill2 == true then
+        self.aniSkill02:update(dt)
+        if(self.tempo > self.tempoaux2 + 2.7) then
+            self.tempoaux2 = 0
+            self.Skill2 = false
+        end
+    end
+
 end
 
 function Hero:mov(dt)
